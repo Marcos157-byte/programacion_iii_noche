86ce0c8415fa348bdb21ba2bb8d5abf900f95192
Marcos157-byte <cmpaolo8@gmail.com>
2025-10-01 21:35:48 -0500

    inicio rama semana_1

--- modulo_1/git/app.py
+++ modulo_1/git/app.py
@@ -2,3 +2,4 @@ print("Bienvenido al Semestre 3")
 print("Programacion III")
 
 print("hola desde ute")
+print("Yo soy un superprogramador")
---
+++ modulo_1/git/app_second_file_mp.py
@@ -0,0 +1,4 @@
+print("My example") 
+print("My project")
+
+print("hola mundo")
